3e189b36af691171f2cfc2385ad7765ecc144a77
Alex Rybak <alexrybak.pr@gmail.com>
2018-12-26 15:51:43 +0200

    added swagger with dump docs

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const argv = require('minimist')(process.argv.slice(2));
+const swagger = require('swagger-node-express');
 const app = express();
 const db = require('./config/db');
 const port = 8000;
+
+// swagger start
+
+const subpath = express();
+app.use(bodyParser());
+app.use('/v1', subpath);
+swagger.setAppHandler(subpath);
+
+app.use(express.static('dist'));
+
+swagger.setApiInfo({
+  title: 'React trello API',
+  description: 'API for simple trello clone react app',
+  termsOfServiceUrl: '',
+  contact: 'alexrybak.pr@gmail.com',
+  license: 'MIT',
+  licenseUrl: ''
+});
+
+subpath.get('/', function(req, res) {
+  res.sendfile(__dirname + '/dist/index.html');
+});
+
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+const domain = 'localhost';
+if (argv.domain !== undefined) domain = argv.domain;
+else
+  console.log(
+    'No --domain=xxx specified, taking default hostname "localhost".'
+  );
+var applicationUrl = 'http://' + domain;
+swagger.configure(applicationUrl, '1.0.0');
+
+// swagger end
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
