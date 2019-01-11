7392aca4325f6b1a8bde24475a4838ba1764c3f9
Alex Rybak <alexrybak.pr@gmail.com>
2019-01-11 16:09:54 +0200

    added cors; removed swagger; added correct error descriptions to auth routes

--- server.js
+++ server.js
@@ -10,39 +10,8 @@ const port = 8000;
 
 // swagger start
 
-const subpath = express();
 app.use(cors());
 app.use(bodyParser());
-app.use('/v1', subpath);
-swagger.setAppHandler(subpath);
-
-app.use(express.static('dist'));
-
-swagger.setApiInfo({
-  title: 'React trello API',
-  description: 'API for simple trello clone react app',
-  termsOfServiceUrl: '',
-  contact: 'alexrybak.pr@gmail.com',
-  license: 'MIT',
-  licenseUrl: ''
-});
-
-subpath.get('/', function(req, res) {
-  res.sendfile(__dirname + '/dist/index.html');
-});
-
-swagger.configureSwaggerPaths('', 'api-docs', '');
-
-const domain = 'localhost';
-if (argv.domain !== undefined) domain = argv.domain;
-else
-  console.log(
-    'No --domain=xxx specified, taking default hostname "localhost".'
-  );
-var applicationUrl = 'http://' + domain;
-swagger.configure(applicationUrl, '1.0.0');
-
-// swagger end
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
