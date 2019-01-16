0fc80f22378d98ee5763b5cf92a600897627d1f5
Alex Rybak <alexrybak.pr@gmail.com>
2019-01-16 13:13:30 +0200

    added full board details url

--- app/routes/board_routes.js
+++ app/routes/board_routes.js
@@ -1,29 +1,12 @@
+const columnRoutes = require('./column_routes');
 const ObjectID = require('mongodb').ObjectID;
 const { tokenGenerator } = require('../encryption/index');
 const { AUTH_CONSTANTS } = require('../CONSTANTS');
 const PREFIX_URL = '/board';
 const COLLECTION_NAME = 'boards';
+const COLUMNS_COLLECTION_NAME = 'columns';
 
 module.exports = function(app, db) {
-  app.post(PREFIX_URL, (req, res) => {
-    const name = req.body.name;
-
-    const email = tokenGenerator.getValidUserByToken(
-      req.headers[AUTH_CONSTANTS.AUTH_HEADER_NAME]
-    );
-
-    db.collection(COLLECTION_NAME).insert(
-      { name: name, owner: email },
-      (err, result) => {
-        if (err) {
-          res.send({ error: 'An error has occurred' });
-        } else {
-          res.send(parseBoardItemFromDB(result.ops[0]));
-        }
-      }
-    );
-  });
-
   app.get(PREFIX_URL + '/list', (req, res) => {
     const email = tokenGenerator.getValidUserByToken(
       req.headers[AUTH_CONSTANTS.AUTH_HEADER_NAME]
@@ -41,6 +24,56 @@ module.exports = function(app, db) {
           res.send(result.map(parseBoardItemFromDB));
         }
       });
+  });
+
+  app.get(PREFIX_URL + '/:id/details', (req, res) => {
+    const email = tokenGenerator.getValidUserByToken(
+      req.headers[AUTH_CONSTANTS.AUTH_HEADER_NAME]
+    );
+    const query = { _id: new ObjectID(req.params.id), owner: { $eq: email } };
+    const columnsQuery = { boardId: req.params.id };
+    // const data = { name, order, tasks, boardId };
+
+    db.collection(COLLECTION_NAME).findOne(query, (err, result) => {
+      if (err) {
+        res.send({ error: 'An error has occurred' });
+      } else {
+        db.collection(COLUMNS_COLLECTION_NAME)
+          .find(columnsQuery)
+          .toArray((err, result) => {
+            if (err) {
+              res.send({ error: 'An error has occurred' });
+            } else if (result) {
+              // res.send(parseColumnItemFromDB(result));
+              res.send({
+                boardId: columnsQuery.boardId,
+                columns: result
+              });
+            } else {
+              res.sendStatus(404);
+            }
+          });
+      }
+    });
+  });
+
+  app.post(PREFIX_URL, (req, res) => {
+    const name = req.body.name;
+
+    const email = tokenGenerator.getValidUserByToken(
+      req.headers[AUTH_CONSTANTS.AUTH_HEADER_NAME]
+    );
+
+    db.collection(COLLECTION_NAME).insert(
+      { name: name, owner: email },
+      (err, result) => {
+        if (err) {
+          res.send({ error: 'An error has occurred' });
+        } else {
+          res.send(parseBoardItemFromDB(result.ops[0]));
+        }
+      }
+    );
   });
 
   app.get(PREFIX_URL + '/:id', (req, res) => {
